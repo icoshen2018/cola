73fde022ab509b945ba07366decc7ec9c0d246d1
icoshen2018 <ppeng881229@126.com>
2018-04-26 16:07:52 +0800

    bit airo

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+typedef struct {
+	unsigned uta:1;
+	unsigned utb:1;
+}tt_test;
+
 int main()
 {
+	tt_test tt;
 	printf("Hello world.\n");
+	printf("Hello text""\x0a"
+		"\x0a"
+		"-t a test""\x0a");
 	return 0;
 }
